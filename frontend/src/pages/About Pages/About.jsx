5439021d23d62079e289c5d8fce800c58396b47b
Daniyal khattak <khattakd87@gmail.com>
2026-06-22 18:20:28 +0500

    Updated About page

--- frontend/src/pages/About Pages/About.jsx
+++ frontend/src/pages/About Pages/About.jsx
@@ -1,4 +1,3 @@
-
 import img5 from "../../images/img5.jpg";
 import img2 from "../../images/img2.jpg";
 import img6 from "../../images/img6.png";
@@ -9,25 +8,26 @@ const About = ({isOpen , setIsOpen}) => {
 
       {/* First Part */}
       <div
-  style={{
-    backgroundImage: `url(${img5})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    height: "34vh",
-  }}
-  className="flex items-center justify-center relative"
->
+        style={{
+        backgroundImage: `url(${img5})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "34vh",
+     }}
+        className="flex items-center justify-center relative"
+        >
   <div className="absolute inset-0 bg-blue-300/40"></div>
   <h1 className="relative text-white text-3xl font-extrabold z-10">About Page</h1>
 </div>
 
-      {/* Second Part */}
-      <div className="flex flex-row p-4 gap-6 md:gap-10 mt-10">
+     <div className="lg:ml-100">
+        {/* Second Part */}
+      <div className="flex flex-row p-4 gap-6 md:gap-10 mt-10 lg:gap-34">
         <div>
-          <h3 className="text-orange-400 font-extrabold">About Us</h3>
-          <h2 className="text-amber-700 font-bold">Welcome to Hunting Era</h2>
+          <h3 className="text-orange-400 font-extrabold lg:text-5xl">About Us</h3>
+          <h2 className="text-amber-700 font-bold lg:text-4xl">Welcome to Hunting Era</h2>
 
-          <p className="text-[10px] text-justify max-w-xs leading-6">
+          <p className="text-[10px] text-justify max-w-xs leading-6 lg:text-lg lg:max-w-lg lg:leading-8">
             Hunting Era is a comprehensive website that helps users find the
             best deals on products, analyze keywords, and locate vendors. Our
             platform allows users to easily search for competitors of different
@@ -35,7 +35,7 @@ const About = ({isOpen , setIsOpen}) => {
             them make more informed purchasing decisions.
           </p>
 
-          <ul className="grid grid-cols-2 gap-4 pt-4 text-xs">
+          <ul className="grid grid-cols-2 gap-4 pt-4 text-xs  lg:gap-6 lg:text-2xl">
             <li>Find Trendings</li>
             <li>Find Compitators</li>
             <li>Find Vendors</li>
@@ -47,29 +47,29 @@ const About = ({isOpen , setIsOpen}) => {
           <img
             src={img2}
             alt="image"
-            className="w-90 h-60 md:w-78 md:h-64"
+            className="w-90 h-60 md:w-78 md:h-64 lg:w-[600px] lg:h-[420px]"
           />
         </div>
       </div>
 
       {/* Third Part */}
-      <div className="flex gap-6 p-4 mt-10">
+      <div className="flex gap-6 p-4 mt-10 lg:gap-34">
 
         <div>
           <img
             src={img5}
             alt="hands and desk"
-            className="w-90 h-60 md:w-74 md:h-64"
+            className="w-90 h-60 md:w-74 md:h-64 lg:w-[600px] lg:h-[420px]"
           />
         </div>
 
         <div className="flex flex-col">
-          <h2 className="text-sm text-justify max-w-sm text-amber-700 font-bold">
+          <h2 className="text-sm text-justify max-w-sm text-amber-700 font-bold lg:text-4xl lg:max-w-lg">
             We Are Committed To Provide<br />
             Safe Purchasing Environment
           </h2>
 
-          <p className="text-[10px] text-justify max-w-xs leading-6">
+          <p className="text-[10px] text-justify max-w-xs leading-6 lg:text-lg  lg:max-w-lg lg:leading-8">
             At Hunting Era, we are committed to providing our users with the best possible
             experience. Whether you are a consumer looking for the best deal on a product
             or a business looking to improve your online presence, Hunting Era has the tools
@@ -79,11 +79,13 @@ const About = ({isOpen , setIsOpen}) => {
             for their products.
           </p>
 
-          <h2 className="text-sm text-yellow-600">Need Any Help? 24/7</h2>
-          <h3 className="text-xs">+92 3025454696</h3>
+          <h2 className="text-sm text-yellow-600 lg:text-xl lg:mt-4">Need Any Help? 24/7</h2>
+          <h3 className="text-xs lg:text-base">+92 3025454696</h3>
         </div>
 
       </div>
+
+     </div>
 
       {/* Fourth Part  */}
       <div
@@ -99,7 +101,6 @@ const About = ({isOpen , setIsOpen}) => {
 
         <div className="absolute inset-0 bg-black/40"></div>
 
-     
         <div className="relative text-center text-white">
           <h1 className="font-bold text-4xl">
             Do You Want to Earn<br />
